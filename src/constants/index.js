0f02b40ea879ea1f7ea2e405adec8b11c45b9e4e
newBJH <bbaek12@gmail.com>
2024-03-23 18:00:10 +0900

    OpenWeatherMap api 정보

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const API_KEY = "449aa65b2dd4d9bacf7a54bb60433592";
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+export const API_KEY_KO = "1GqQRJlRR2GqkESZURdhSw"
+export const BASE_URL_KO = "https://apihub.kma.go.kr/api/typ01/cgi-bin/url/nph-aws2_min"
+
+
+//forecasts
+export const API_KEY_BJH = "15b326c9c3c7497f23a213fb964f6716";
+export const BASE_URL_FORECAST = "https://api.openweathermap.org/data/2.5/forecast"; 
+
+
