c50d9a4c19c1f29ff4403b8b2d693cd0113e0c49
newBJH <bbaek12@gmail.com>
2024-11-12 00:40:22 +0900

    pinia import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import './assets/main.css';
+import '@/index.css'
+
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createPinia } from 'pinia'
+import fetchAndSaveProjectStats from '@/utils/fetchProjectStats.js'
+import fetchAndSaveUserLocation from '@/utils/fetchUserLocation.js'
+import onlineHandler from '@/utils/onlineHandler.js'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 import Chart from 'primevue/chart';
@@ -9,8 +13,12 @@ import Chart from 'primevue/chart';
 import 'primeicons/primeicons.css'; // PrimeIcons
 import 'primeflex/primeflex.css'; // PrimeFlex
 
+import App from './App.vue';
+import router from './router';
+
 const app = createApp(App);
 
+app.use(createPinia())
 app.use(router);
 app.use(Chart);
 app.use(PrimeVue, {
@@ -20,3 +28,7 @@ app.use(PrimeVue, {
 });
 
 app.mount('#app');
+
+fetchAndSaveUserLocation()
+fetchAndSaveProjectStats()
+onlineHandler()
